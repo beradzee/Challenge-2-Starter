827f75a717f597fc90b07cd58829bb941e8ebebf
beradzee <116104845+beradzee@users.noreply.github.com>
2024-10-07 21:01:12 +0400

    feauter/footer
    adding links

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import Navbar from './components/Navbar';
+import React from "react";
+import Navbar from "./components/Navbar";
+import Foot from "./components/Foot";
 
 const App = () => {
   return (
@@ -7,6 +8,11 @@ const App = () => {
       <div className="flex items-center justify-center px-6 sm:px-16">
         <div className="max-w-[1280px] w-full">
           <Navbar />
+        </div>
+      </div>
+      <div className="flex items-center justify-center px-6 sm:px-16 bg-primary sm:px16">
+        <div className="max-w-[1280px] w-full">
+          <Foot />
         </div>
       </div>
     </div>
